8ea013c71762d85f42c58ed78e958d820ae1badc
haythem-said <haythem.said97@gmail.com>
2023-07-05 23:59:50 +0100

    concept general

--- src/pages/generalConcept/GeneralConcept.js
+++ src/pages/generalConcept/GeneralConcept.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import SideBarComponent from "../../components/sideBar/SideBarComponent";
+import Header from "../../components/header/Header";
 
 const GeneralConcept = () => {
   return (
-    <div>
-        <h1>Welcome to general concept</h1>
-    </div>
-  )
-}
+    <>
+      <SideBarComponent />
+      <Header />
 
-export default GeneralConcept
+      <div className=" general-concept">
+        <img src="https://pbs.twimg.com/media/FMIRYHOWUAQPdTr.jpg" />
+      </div>
+    </>
+  );
+};
+
+export default GeneralConcept;
